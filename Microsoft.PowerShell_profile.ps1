3a60a060b9b7045a80d166ceeb5a6bb43ada9882
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2025-04-07 19:17:45 +0900

    Add files via upload

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,15 @@
 Function a {
     Start-Process https://amazon.co.jp
 }
+Function c {
+    Start-Process https://gemini.google.com
+}
+Function ca {
+    Start-Process https://calendar.google.com
+}
 Function g {
-    Start-Process "https://github.com/mxvish?tab=repositories"
+    Start-Process https://github.com/mxvish?tab=repositories
+}
+Function ke {
+    Start-Process https://keep.google.com
 }
