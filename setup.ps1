08c22a0bae66d6830d5f8bf1b052f766086d42e9
Kenta Oshima <oshiken95@gmail.com>
2025-11-13 13:43:40 +0900

    feat: install rufus

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,3 @@
-winget install --accept-package-agreements Git.Git GitHub.GitHubDesktop Google.GoogleDrive Google.JapaneseIME Microsoft.Teams Mozilla.Firefox Notepad++.Notepad++ powershell python Mozilla.Thunderbird vim.vim winch
+winget install --accept-package-agreements Git.Git GitHub.GitHubDesktop Google.GoogleDrive Google.JapaneseIME Microsoft.Teams Mozilla.Firefox Notepad++.Notepad++ powershell python rufus.rufus Mozilla.Thunderbird vim.vim winch
 git clone --depth=1 https://github.com/github/copilot.vim.git `
   $HOME/vimfiles/pack/github/start/copilot.vim
