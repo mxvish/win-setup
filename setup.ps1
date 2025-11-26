6dd032493cb4798d580f40afb0ec6afe68c4ce44
Kenta Oshima <oshiken95@gmail.com>
2025-11-26 16:23:34 +0900

    feat: setup powershell profile

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,4 @@
 winget install --accept-package-agreements Git.Git GitHub.GitHubDesktop Google.GoogleDrive Google.JapaneseIME Microsoft.Teams Mozilla.Firefox Notepad++.Notepad++ ollama powershell python rufus.rufus Mozilla.Thunderbird vim.vim winch
 git clone --depth=1 https://github.com/github/copilot.vim.git `
   $HOME/vimfiles/pack/github/start/copilot.vim
+mv Microsoft.PowerShell_profile.ps1 ~\Documents\PowerShell\
