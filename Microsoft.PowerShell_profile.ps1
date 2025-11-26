9b7c664d6b763d7d6fe6d15b6dada7df94ca12ca
Kenta Oshima <oshiken95@gmail.com>
2025-11-26 16:27:28 +0900

    feat: set 2 key binds

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -124,6 +124,8 @@ Function y {
 	Start-Process https://youtube.com/feed/playlists
 }
 
+Set-PSReadLineKeyHandler -Chord Ctrl-a -Function BeginningOfLine
+Set-PSReadLineKeyHandler -Chord Ctrl-e -Function EndOfLine
 Set-PSReadLineKeyHandler -Key Ctrl+j -Function AcceptLine
 Set-PSReadLineKeyHandler -Key Ctrl+n -Function NextHistory
 Set-PSReadLineKeyHandler -Key Ctrl+p -Function PreviousHistory
