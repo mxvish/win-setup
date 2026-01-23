3b536ddbd49f8d51fd1963371e7d5cb0c48b9898
Kenta Oshima <oshiken95@gmail.com>
2026-01-23 23:20:24 +0900

    feat: install lf

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,4 @@
-winget install --accept-package-agreements btop fastfetch Git.Git GitHub.GitHubDesktop Google.GoogleDrive Google.JapaneseIME Microsoft.Teams Mozilla.Firefox Notepad++.Notepad++ ollama microsoft.powershell PDFLabs.PDFtk.Free python rufus.rufus Mozilla.Thunderbird vim.vim winch
+winget install --accept-package-agreements btop fastfetch Git.Git GitHub.GitHubDesktop Google.GoogleDrive Google.JapaneseIME gokcehan.lf Microsoft.Teams Mozilla.Firefox Notepad++.Notepad++ ollama microsoft.powershell PDFLabs.PDFtk.Free python rufus.rufus Mozilla.Thunderbird vim.vim winch
 git clone --depth=1 https://github.com/github/copilot.vim.git `
   $HOME/vimfiles/pack/github/start/copilot.vim
 mkdir ~\Documents\PowerShell
