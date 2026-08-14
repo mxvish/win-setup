7387c7e0a259636ec8f2d1e5ded26e019a8ac2d7
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2026-08-14 02:55:40 -0700

    feat: uninstall onedrive

--- setup.ps1
+++ setup.ps1
@@ -3,6 +3,8 @@ Set-WinUILanguageOverride en-US
 Set-Culture en-US
 Set-WinHomeLocation -GeoId 244
 
+winget uninstall onedrive
+
 winget install --accept-package-agreements btop fastfetch Git.Git GitHub.GitHubDesktop Google.GoogleDrive Google.JapaneseIME gokcehan.lf Microsoft.Teams Mozilla.Firefox Notepad++.Notepad++ ollama microsoft.powershell microsoft.VisualStudioCode PDFLabs.PDFtk.Free python rufus.rufus Mozilla.Thunderbird vim.vim winch --source winget
 winget install --accept-package-agreements XPFCC4CD725961 #line
 
