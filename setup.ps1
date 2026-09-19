41faca529666954016a3f661d128fff63e7f2be3
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2026-09-19 03:21:39 -0600

    feat: install steam

--- setup.ps1
+++ setup.ps1
@@ -5,7 +5,7 @@ Set-WinHomeLocation -GeoId 244
 
 winget uninstall onedrive
 
-winget install --accept-package-agreements btop fastfetch Git.Git GitHub.GitHubDesktop Google.GoogleDrive Google.JapaneseIME gokcehan.lf Microsoft.Teams Mozilla.Firefox Notepad++.Notepad++ ollama microsoft.powershell microsoft.VisualStudioCode neovim PDFLabs.PDFtk.Free python rufus.rufus Mozilla.Thunderbird vesktop winch --source winget
+winget install --accept-package-agreements btop fastfetch Git.Git GitHub.GitHubDesktop Google.GoogleDrive Google.JapaneseIME gokcehan.lf Microsoft.Teams Mozilla.Firefox Notepad++.Notepad++ ollama microsoft.powershell valve.steam microsoft.VisualStudioCode neovim PDFLabs.PDFtk.Free python rufus.rufus Mozilla.Thunderbird vesktop winch --source winget
 winget install --accept-package-agreements XPFCC4CD725961 #line
 
 winget upgrade -r
